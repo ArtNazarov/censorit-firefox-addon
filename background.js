5e5ab56f5ec887397cb29ad9234b2dddbc63e2d1
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-09-09 19:53:45 +0500

    Added password protection for dialog

--- background.js
+++ background.js
@@ -1,18 +1,46 @@
-browser.runtime.onMessage.addListener((message) => {
-    
-    if (message.tags !== undefined) {
-        console.log("Теги:", message.tags);
-        browser.storage.local.set({ 'tags': message.tags }, () => {
-        console.log('Tags saved:', message.tags);
-        });
-    };
-
-
+function onSaveWhiteList(message){
     if (message.whitelist !== undefined) {
         console.log("Белый список:", message.whitelist);
         browser.storage.local.set({ 'whitelist': message.whitelist }, () => {
         console.log('Whitelist saved:', message.whitelist);
         });
     }
+}
+
+function onSaveTags(message){
+    if (message.tags !== undefined) {
+        console.log("Теги:", message.tags);
+        browser.storage.local.set({ 'tags': message.tags }, () => {
+        console.log('Tags saved:', message.tags);
+        });
+    };
+}
+
+function onSavePassword(message){
+    if (message.password !== undefined) {
+        console.log("Пароль:", message.password);
+        browser.storage.local.set({ 'password': message.password }, () => {
+        console.log('The password saved:', message.password);
+        });
+    };
+}
+
+
+function onSaveState(message){
+    if (message.state !== undefined) {
+        console.log("Состояние:", message.state);
+        browser.storage.local.set({ 'state': message.state }, () => {
+        console.log('The state saved:', message.state);
+        });
+    };
+}
+
+browser.runtime.onMessage.addListener((message) => {
+    
+    onSaveWhiteList(message);
+    onSaveTags(message);
+    onSavePassword(message);
+    onSaveState(message);
+
 
 });
--- popup.js
+++ popup.js
@@ -116,20 +116,35 @@ document.getElementById('mysticFilters').onclick = function(e){
   loadFromUrlToApp(MysticUrl);
 }
 
+function makeVisibleByState(state){
+   console.log(state);
+    if (state === "UNLOCKED" || state === undefined){
+      document.getElementById("main_form").style.display="block";
+      document.getElementById("unlock_password_form").style.display="none";
+    } else {
+      document.getElementById("main_form").style.display="none";
+      document.getElementById("unlock_password_form").style.display="block";
+    };
+}
+
 // Считываем значение настройки после загрузки диалога
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
     // теги в форму
-    browser.storage.local.get('tags').then((data) => {
-        if (data.tags) {
-            document.getElementById('tags').value = data.tags;
-        }
-    });
+    let dataTags = await browser.storage.local.get('tags');
+    document.getElementById('tags').value = dataTags.tags;
     // белый список в форму
-    browser.storage.local.get('whitelist').then((data) => {
-      if (data.whitelist) {
-          document.getElementById('whitelist').value = data.whitelist;
-      }
-  });
+    let dataWhiteList = await browser.storage.local.get('whitelist');
+    document.getElementById('whitelist').value = dataWhiteList.whitelist;
+    let dataState = await browser.storage.local.get("state");
+    document.getElementById('state').value = dataState.state;
+    let dataPassword = await browser.storage.local.get("password");
+    document.getElementById("password1").value = dataPassword.password;
+    document.getElementById("password2").value = dataPassword.password;
+    makeVisibleByState(dataState.state);
+  } catch (error) {
+    console.log("Error", error);
+  }
 });
 
 // Обработка сохранения формы
@@ -141,4 +156,26 @@ document.getElementById('saveButton').addEventListener('click', () => {
     const myWhiteList = document.getElementById('whitelist').value;
     browser.runtime.sendMessage({ whitelist: myWhiteList });
 
+});
+
+// Изменение пароля
+document.getElementById('btnSetPassword').addEventListener('click', ()=>{
+  const password1 = document.getElementById('password1').value;
+  const password2 = document.getElementById('password2').value;
+  if (password1 == password2){
+    browser.runtime.sendMessage({password: password1});
+    console.log('Password changed to ', password1);
+    browser.runtime.sendMessage({state: "LOCKED"});
+  };
+});
+
+// Вход в расширение
+document.getElementById('btnLogin').addEventListener('click', async()=>{
+  const password = document.getElementById('password').value;
+  const dataExpectedPassword = await browser.storage.local.get("password");
+  if (password == dataExpectedPassword.password){
+    const state = "UNLOCKED";
+    browser.runtime.sendMessage({state: state});
+    makeVisibleByState(state);
+  };
 });
